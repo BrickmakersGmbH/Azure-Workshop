2fffe1cbc1f4fde3a7beca4fdcdf852097a8fa5d
Stefan Griebel <stef.griebel@googlemail.com>
2019-04-11 15:15:47 +0200

    Create UploadController and view with simple design first

---
+++ CatDogCore/CatDogCore/Controllers/UploadController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CatDogCore.Models;
+using CatDogCore.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatDogCore.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IBlobstorageService _blobStorageService;
+
+        public UploadController(IBlobstorageService blobStorageService)
+        {
+            _blobStorageService = blobStorageService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var paths = await _blobStorageService.GetFiles("uploads");
+            ViewData["Images"] = paths.ToArray();
+            return View();
+        }
+
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            foreach (var formFile in files)
+            {
+                using (var stream = formFile.OpenReadStream())
+                {
+                    await _blobStorageService.AddFile("uploads", stream);
+                }
+            }
+
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
