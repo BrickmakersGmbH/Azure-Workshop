c78fb4c9cfc4e9d40d8a68e5a1888a117c958ff5
Stefan Griebel <stef.griebel@googlemail.com>
2019-04-12 12:16:05 +0200

    Create LinqExtension for partitionating image list in subsets

---
+++ CatDogCore/CatDogCore/Extensions/LinqExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CatDogCore.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Partition<T>(this IEnumerable<T> enumerable, int partitionSize)
+        {
+            return enumerable
+                .Select((item, index) => new { Index = index, Item = item })
+                .GroupBy(indexedItem => indexedItem.Index / partitionSize)
+                .Select(group => group.Select(indexedItem => indexedItem.Item));
+        }
+    }
+}
